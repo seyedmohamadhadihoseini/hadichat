d424cd9d2608e5e6a35ce5c27a05041191247329
hadi <seyedmohadihoseini@gmail.com>
2025-09-18 09:15:36 +0330

    fix nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,22 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
     images:{
         remotePatterns:[
             {
                 hostname:"localhost"
+            },{
+                hostname:"chat.zilab.ru"
             }
         ]
     },
-    
+    experimental:{
+        serverActions:{
+            allowedOrigins:["chat.zilab.ru","*chat.zilab.ru"]
+        },
+        
+    },
+    allowedDevOrigins:["chat.zilab.ru","*chat.zilab.ru"]
 };
 
 export default nextConfig;
